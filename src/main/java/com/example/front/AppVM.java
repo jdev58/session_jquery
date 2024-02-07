7af1a6a4ca7c2d829a31ae08ebcf57ba27253fa0
Mohammad Tafreshi <mn.tafreshi@gmail.com>
2024-02-07 09:15:45 +0330

    session qjuryr

---
+++ src/main/java/com/example/front/AppVM.java
@@ -0,0 +1,14 @@
+package com.example.front;
+
+public class AppVM {
+    private String name;
+
+    public void setName(String name ){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+}
